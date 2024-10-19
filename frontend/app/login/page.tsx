5c06306ba7a1843a2c9ddfbaa01664864ea0f7fc
Chris Runo <chris.runo@gmail.com>
2024-10-19 09:37:38 -0400

    Add middleware and action for pocketbase authentication with dashboard for logged in users.

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import { login } from '../actions';
 
 export default function Login() {
 	return (
 		<div className="flex items-center justify-center h-screen">
 			<div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
 				<h2 className="text-2xl font-semibold mb-4">Login</h2>
-				<form>
+				<form action={login}>
 					<div className="mb-4">
 						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
 							Email
