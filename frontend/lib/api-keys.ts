e9a6e31b30dcda482fdbe73ddb75a49818348969
Chris Runo <chris.runo@gmail.com>
2024-10-26 07:41:39 -0400

    Fix type error for api-keys library.

--- frontend/lib/api-keys.ts
+++ frontend/lib/api-keys.ts
@@ -5,7 +5,7 @@ export function generateApiKey() {
 	return crypto.randomBytes(32).toString('hex')
 }
 
-export async function hashApiKey(apiKey) {
+export async function hashApiKey(apiKey: string) {
 	const salt = await bcrypt.genSalt(10);
 	return bcrypt.hash(apiKey, salt);
 }
