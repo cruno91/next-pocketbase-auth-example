b75242601eb9c6af8952aac46f8a90f8815040c7
Chris Runo <chris.runo@gmail.com>
2024-10-27 20:05:59 -0400

    Add config command for api server.

---
+++ server/cmd/config.go
@@ -0,0 +1,61 @@
+// cmd/serve.go
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	PbUrl   string
+	PbPw    string
+	PbEmail string
+)
+
+// configCmd represents the serve command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Set Viper configuration values
+		viper.Set("pocketbase.url", PbUrl)
+		viper.Set("pocketbase.password", PbPw)
+		viper.Set("pocketbase.email", PbEmail)
+
+		// Try to write the configuration to an existing config file or create one if it doesn't exist
+		if err := viper.SafeWriteConfig(); err != nil {
+			// SafeWriteConfig returns an error if the file already exists, so we use WriteConfig instead
+			var configFileAlreadyExistsError viper.ConfigFileAlreadyExistsError
+			if errors.As(err, &configFileAlreadyExistsError) {
+				err = viper.WriteConfig()
+			}
+		}
+
+		// Write the changes to the configuration file
+		err := viper.WriteConfig()
+		if err != nil {
+			fmt.Printf("Error writing config file: %v\n", err)
+		} else {
+			fmt.Println("Configuration saved successfully.")
+		}
+	},
+}
+
+func init() {
+	// Register configCmd with the root command
+	rootCmd.AddCommand(configCmd)
+
+	// Add flags for configuration settings
+	configCmd.Flags().StringVarP(&PbUrl, "url", "u", "http://db.authexample.lndo.site", "Pocketbase URL")
+	configCmd.Flags().StringVarP(&PbPw, "password", "p", "In5ecureP4S123", "Pocketbase password")
+	configCmd.Flags().StringVarP(&PbEmail, "email", "e", "admin@example.com", "Pocketbase email")
+
+	// Bind Viper to each flag to allow loading them directly
+	viper.BindPFlag("pocketbase.url", configCmd.Flags().Lookup("url"))
+	viper.BindPFlag("pocketbase.password", configCmd.Flags().Lookup("password"))
+	viper.BindPFlag("pocketbase.email", configCmd.Flags().Lookup("email"))
+}
--- server/cmd/root.go
+++ server/cmd/root.go
@@ -4,9 +4,15 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
 	"os"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	cfgFile string
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -34,13 +40,40 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	cobra.OnInitialize(initConfig)
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.next-auth-example.yaml)")
+	// Here you will define your flags and configuration settings.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fulcrum.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in both the home directory and the current working directory
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(".") // Add current working directory as a config path
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".authexample")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintf(os.Stderr, "Using config file: %s\n", viper.ConfigFileUsed())
+	} else {
+		// Optionally, create the file if it doesn't exist yet
+		fmt.Println("No existing config file found. A new one will be created on save.")
+	}
 }
